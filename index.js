ddbde6fd64f32b2121dadf93e8b6684fe8c47d3e
Maxime Caly <max.caly@gmail.com>
2019-03-03 14:29:08 -0800

    Clean up project and reorganize event emitter with StarcraftManager

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const express = require('express');
 const axios = require('axios');
-const { EventEmitter } = require('events');
 const fs = require('fs');
+const StarcraftManager = require('./libs/StarcraftManager');
 
 const app = express();
-const port = 3000
+const port = 3000;
 const routes = require('./routes/');
 
-const scEventEmitter = new EventEmitter;
-
 const clientGameURL = 'http://localhost:6119/game';
-const clientUiURL = 'http://localhost:6119/ui';
-const unmaskedURL = 'http://sc2unmasked.com/API/Player?'
+const unmaskedURL = 'http://sc2unmasked.com/API/Player?';
 
 const conf = require('./configs/conf.json');
 
-let previousGameState = "inMenu";
+const starcraftManager = new StarcraftManager();
 
 app.use(express.static('public'));
 app.use('/api', routes);
 
-app.listen(port, () => console.log(`Listening on port: ${port}!`))
+app.listen(port, () => console.log(`Listening on port: ${port}!`));
 
+function writeFile(file, obj) {
+  fs.writeFile(file, obj, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('The file was written to.');
+  });
+}
 
 async function getClientData() {
   try {
-    let { data } = await axios.get(clientGameURL);
+    const { data } = await axios.get(clientGameURL);
     return data;
   } catch (err) {
     throw new Error('No game data available from the starcraft 2 client server');
@@ -44,10 +49,10 @@ function processClientData(data) {
         },
         {
           name: data.players[1].name,
-          race: data.players[1].race
-        }
-      ]
-    }
+          race: data.players[1].race,
+        },
+      ],
+    };
 
     return gameInfo;
   }
@@ -58,32 +63,28 @@ function processClientData(data) {
  * Allows to get the data of both players on unmasked and potentially unmask a barcode or smurf
  */
 async function getUnmaskedData(gameInfo) {
-  let queries = [];
-  gameInfo.players.forEach(player => {
-    console.log(player)
-    queries.push(`${unmaskedURL}name=${player.name}&race=${player.race[0]}&server=${gameInfo.server}`)
+  const queries = [];
+  gameInfo.players.forEach((player) => {
+    console.log(player);
+    queries.push(`${unmaskedURL}name=${player.name}&race=${player.race[0]}&server=${gameInfo.server}`);
   });
   console.log(queries);
-  let response = await Promise.all(queries.map(query => axios.get(query)));
+  const response = await Promise.all(queries.map(query => axios.get(query)));
   // console.log(response[0].data, response[1].data);
 
   if (response[0].data.players.length > 0 && response[1].data.players.length > 0) {
     // Allows to sort a list of players recognized from unmasked by sorting by last_played. We are taking the player in the list that has most recently played. (Might want to use .filter)
-    response[0].data.players.sort((a, b) => {
-      return b.last_played.replace(/[a-zA-Z()/]*/ig, '') - a.last_played.replace(/[a-zA-Z()/]*/ig, '');
-    })
-    response[1].data.players.sort((a, b) => {
-      return b.last_played.replace(/[a-zA-Z()/]*/ig, '') - a.last_played.replace(/[a-zA-Z()/]*/ig, '');
-    })
+    response[0].data.players.sort((a, b) => b.last_played.replace(/[a-zA-Z()/]*/ig, '') - a.last_played.replace(/[a-zA-Z()/]*/ig, ''));
+    response[1].data.players.sort((a, b) => b.last_played.replace(/[a-zA-Z()/]*/ig, '') - a.last_played.replace(/[a-zA-Z()/]*/ig, ''));
 
 
-    let players = response.reduce((prev, curr) => {
-      let { data } = curr;
+    const players = response.reduce((prev, curr) => {
+      const { data } = curr;
       prev.push({
         acc_name: data.players[0].acc_name,
         displayName: data.players[0].display_name,
-        mmr: data.players[0].mmr
-      })
+        mmr: data.players[0].mmr,
+      });
       return prev;
     }, []);
     return players;
@@ -91,43 +92,27 @@ async function getUnmaskedData(gameInfo) {
   throw new Error('One or more players could not be found');
 }
 
-async function checkGameState() {
-  try {
-    let { data } = await axios.get(clientUiURL);
-    if (data.activeScreens.length === 0 && previousGameState !== "gameJoined") {
-      console.log('emitting gameJoined event')
-      scEventEmitter.emit('gameJoined')
-      previousGameState = "gameJoined"
-    } else if (data.activeScreens.length !== 0 && previousGameState !== "inMenu") {
-      scEventEmitter.emit('gameLeft');
-      previousGameState = "inMenu"
-    }
-  } catch (e) {
-    throw new Error('Starcraft is not running!');
-  }
-}
-
-
-
 /**
- * When a game starts the event emitter emits "gameJoined" event which kickstarts the process to get data about the players.
+ * When a game starts the event emitter emits "game-joined"
+ * event which kickstarts the process to get data about the players.
  */
-scEventEmitter.on('gameJoined', async() => {
+starcraftManager.on('game-joined', async() => {
   console.log('new game entered');
-  let data = await getClientData();
-  let gameInfo = processClientData(data);
-  let unmaskedData = await getUnmaskedData(gameInfo);
+  const data = await getClientData();
+  const gameInfo = processClientData(data);
+  const unmaskedData = await getUnmaskedData(gameInfo);
   writeFile('./data/game-data.json', JSON.stringify(unmaskedData));
-  console.log(unmaskedData)
-})
+  console.log(unmaskedData);
+});
 
 
 /**
- * When a game ends and we return to the menu, thee event emitter emits the "gameLeft" event so we can plug in to that event.
+ * When a game ends and we return to the menu, the event emitter emits the "game-left"
+ * event so we can plug into that event.
  */
-scEventEmitter.on('gameLeft', () => {
+starcraftManager.on('game-left', () => {
   console.log('in menus');
-})
+});
 
 /**
  * Allows to add a retry mechanism to the callback function given to it.
@@ -137,29 +122,19 @@ async function retry(delay = 5000, limit = 30000, callback) {
     try {
       await callback();
       setTimeout(() => {
-        retry(delay, limit, callback)
-      }, delay)
+        retry(delay, limit, callback);
+      }, delay);
     } catch (e) {
-      console.log(e)
+      console.log(e);
       setTimeout(() => {
         delay >= limit ? limit : delay * 2;
         retry(delay, limit, callback);
-      }, delay)
+      }, delay);
     }
   }
 }
 
-retry(5000, 30000, checkGameState)
-
-
-function writeFile(file, obj) {
-  fs.writeFile(file, obj, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log('The file was written to.');
-  });
-}
+retry(5000, 30000, starcraftManager.checkGameState);
 
 // Testing the unmasked function
 // getUnmaskedData(debugInfo).then(data => console.log(data)).catch(e => console.log(e))
---
+++ libs/StarcraftManager.js
@@ -0,0 +1,29 @@
+const { EventEmitter } = require('events');
+const axios = require('axios');
+
+const clientUiURL = 'http://localhost:6119/ui';
+
+class StarcraftManager extends EventEmitter {
+  constructor() {
+    super();
+    this.previousGameState = 'in-menu';
+  }
+
+  async checkGameState() {
+    try {
+      const { data } = await axios.get(clientUiURL);
+      if (data.activeScreens.length === 0 && this.previousGameState !== 'gameJoined') {
+        console.log('emitting game-joined event');
+        this.emit('game-joined');
+        this.previousGameState = 'game-joined';
+      } else if (data.activeScreens.length !== 0 && this.previousGameState !== 'inMenu') {
+        this.emit('game-left');
+        this.previousGameState = 'in-menu';
+      }
+    } catch (e) {
+      throw new Error('Starcraft is not running!');
+    }
+  }
+}
+
+module.exports = StarcraftManager;

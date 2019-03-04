d10ec1275eaf707d172f261a3df1af6949812588
Maxime Caly <max.caly@gmail.com>
2019-03-03 16:26:56 -0800

    Change css and run eslint on client side

--- public/main.js
+++ public/main.js
@@ -1,37 +1,36 @@
-const gameInfoURL = "http://localhost:3000/api/gameInfo";
+const gameInfoURL = 'http://localhost:3000/api/gameInfo';
 
 let glitchTextEl;
 let containerEl;
 
-document.addEventListener("DOMContentLoaded", async(e) => {
+document.addEventListener('DOMContentLoaded', async (e) => {
   containerEl = document.querySelector('.container');
   glitchTextEl = document.querySelectorAll('.glitch');
-})
+});
 
 async function getGameInfo() {
-  let response = await fetch(gameInfoURL);
-  return await response.json();
+  const response = await fetch(gameInfoURL);
+  return response.json();
 }
 
 // When game state changes (/UI on starcraft client ) call getGameInfo again
 // When out of game =>  containerEl.classList.toggle('fade')
 
-
 async function renderPlayerInfos() {
-  let players = await getGameInfo();
-  let [player1, player2]= [...players];
+  const players = await getGameInfo();
+  const [player1, player2] = [...players];
 
-  player1.name = player1.displayName  ? player1.displayName : player1.acc_name;
-  player2.name = player2.displayName  ? player2.displayName : player2.acc_name;
+  player1.name = player1.displayName ? player1.displayName : player1.acc_name;
+  player2.name = player2.displayName ? player2.displayName : player2.acc_name;
 
-  glitchTextEl[0].innerHTML = `${player1.name} (${player1.mmr})`
-  glitchTextEl[0].dataset.text = `${player1.name} (${player1.mmr})`
-  glitchTextEl[2].innerHTML = `${player2.name} (${player2.mmr})`
-  glitchTextEl[2].dataset.text = `${player2.name} (${player2.mmr})`
+  glitchTextEl[0].innerHTML = `${player1.name} (${player1.mmr})`;
+  glitchTextEl[0].dataset.text = `${player1.name} (${player1.mmr})`;
+  glitchTextEl[2].innerHTML = `${player2.name} (${player2.mmr})`;
+  glitchTextEl[2].dataset.text = `${player2.name} (${player2.mmr})`;
 }
 
 renderPlayerInfos();
 
-setInterval(async() => {
+setInterval(async () => {
   renderPlayerInfos();
-}, 10000)
+}, 10000);
